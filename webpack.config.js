100ab1f6078f3e23c14f6a8bceff61db90c600f5
Lina Sanchez <66146560+linafx@users.noreply.github.com>
2021-04-30 21:07:08 -0500

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js"
+  },
+  devServer: {
+    publicPath: "/build/",
+    contentBase: "/build",
+    hot: true,
+    port: 8080,
+    proxy: {
+      "/": "http://localhost:3000",
+      "/investment": "http://localhost:3000"
+    }
+  },
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-flow"
+            ],
+            plugins: ["@babel/plugin-proposal-class-properties"]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {}
+          }
+        ]
+      }
+    ]
+  }
+};
